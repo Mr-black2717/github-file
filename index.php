cb29766c760d04fe56a9d776727143ab2e774c66
Mr-black2717 <balckmonster805@gmail.com>
2025-08-20 05:56:23 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'includes/header.php'; ?>
+<h1>Welcome to Our Pet Shop</h1>
+<p>Your one-stop shop for loving pets and quality pet products!</p>
+<?php include 'includes/footer.php'; ?>
